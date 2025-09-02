efaad81fe2eb38208b0a59e0c58e47fa8ae2a822
Juan Alberto Cáceres Vega <caceresvega@gmail.com>
2025-09-01 19:11:29 -0600

    update

--- app/Services/Hacienda/Login/Token.php
+++ app/Services/Hacienda/Login/Token.php
@@ -54,7 +54,8 @@ class Token
     //Log::debug("getToken", [$username, $password, $this->authUrl]);
     // Verificamos si el access_token es válido
     if ($this->tokenStorage->isAccessTokenValid($username)) {
-      Log::debug("getToken", ['Entrol linea 57']);
+      //Log::debug("getToken", ['Entrol linea 57']);
+      Log::info("getToken linea 58");
       $tokenData = $this->tokenStorage->getTokens($username);
       Log::debug("getToken", [$tokenData]);
       return $tokenData['access_token'];
@@ -62,12 +63,12 @@ class Token
 
     // Si el refresh_token es válido, lo usamos para renovar el access_token
     if ($this->tokenStorage->isRefreshTokenValid($username)) {
-      Log::debug("getToken", ['Entrol linea 65']);
+      Log::info("getToken linea 65");
       $tokenData = $this->tokenStorage->getTokens($username);
       return $this->refreshToken($username, $tokenData['refresh_token']);
     }
 
-    Log::debug("getToken", ['Solicitando nuevo token']);
+    Log::info("getToken Solicitando nuevo token");
     // Si no, solicitamos un nuevo token
     return $this->requestNewToken($username, $password);
   }
